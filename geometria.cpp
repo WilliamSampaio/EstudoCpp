2c1c736e02a02c8269b75b3b201200f3cb1618c6
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-06-08 23:20:39 -0400

    update

--- geometria.cpp
+++ geometria.cpp
@@ -16,6 +16,7 @@ double areaCirculo(double r)
     return M_PI * (r * r);
 }
 
-int fatorial(int n){
-    return (n == 0) ? 1 : n * fatorial(n -1);
+unsigned long int fatorial(unsigned long int n)
+{
+    return (n == 0) ? 1 : n * fatorial(n - 1);
 }
--- geometria.h
+++ geometria.h
@@ -2,4 +2,4 @@ double areaRetangulo(double, double);
 double areaQuadrado(double);
 double areaCirculo(double);
 
-int fatorial(int);
+unsigned long int fatorial(unsigned long int);
